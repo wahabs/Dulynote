566699e0682b18a646c07ed1529254377bc38854
Wahab Sheikh <wahabmsheikh@gmail.com>
2015-02-09 11:59:14 -0800

    Add a notes.css style file

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -1,59 +1,68 @@
-App.Routers.Router = Support.SwappingRouter.extend({
+  App.Routers.Router = Backbone.Router.extend({
 
   initialize : function(options) {
-    this.el = options.$rootEl;
-    this.sideEl = options.$sideEl;
+    this.els = {
+      "#content" : options.$rootEl,
+      "#sidebar" : options.$sideEl
+    };
     this.notebooks = new App.Collections.Notebooks();
     this.notes = new App.Collections.Notes();
     this.tags = new App.Collections.Tags();
+    this.currentViews = {};
+    
   },
 
   routes : {
     "" : "notebooksIndex",
+    "notebooks" : "notebooksIndex",
     "notebooks/:id" : "notebookShow",
-    "notes/:id/edit" : "noteEdit",
     "notes/new" : "noteNew",
+    "notes/:id/edit" : "noteEdit",
     "tags/:id" : "tagShow"
   },
 
   notebooksIndex : function() {
     this.notebooks.fetch();
     this.tags.fetch();
-    var view = new App.Views.NotebooksIndex({ collection: this.notebooks, tags: this.tags });
-    this.swap(view);
+    var view = new App.Views.NotebooksIndex({
+      collection: this.notebooks, tags: this.tags
+    });
+    this.swapView(view, "#sidebar");
   },
 
   notebookShow : function(id) {
     var notebook = this.notebooks.getOrFetch(id);
     var view = new App.Views.NotebookShow({ model: notebook });
-    this.swap(view);
+    this.swapView(view, "#sidebar");
   },
 
   noteEdit : function(id) {
     this.notebooks.fetch();
     this.tags.fetch();
     var note = this.notes.getOrFetch(id);
-    var view = new App.Views.NoteEdit({ model: note, notebooks: this.notebooks, tags: this.tags });
-    this.swap(view);
+    var view = new App.Views.NoteEdit({
+      model: note, notebooks: this.notebooks, tags: this.tags
+    });
+    this.swapView(view, "#content");
   },
 
   tagShow : function(id) {
     var tag = this.tags.getOrFetch(id);
     var view = new App.Views.TagShow({ model: tag });
-    this.swap(view);
+    this.swapView(view, "#sidebar");
   },
 
-  // swapSideView: function(sideView) {
-  //   if (this.currentSide && this.currentSide.leave) {
-  //     this.currentSide.leave();
-  //   }
-  //
-  //   this.currentSide = sideView;
-  //   $(this.sideEl).html(this.currentSide.render().el);
-  //
-  //   if (this.currentSide && this.currentSide.swapped) {
-  //     this.currentSide.swapped();
-  //   }
-  // }
+  swapView : function(newView, selector) {
+    if (this.currentViews[selector] && this.currentViews[selector].leave) {
+      this.currentViews[selector].leave();
+    }
+
+    this.currentViews[selector] = newView;
+    $(this.els[selector]).html(this.currentViews[selector].render().el);
+
+    if (this.currentViews[selector] && this.currentViews[selector].swapped) {
+      this.currentViews[selector].swapped();
+    }
+  }
 
 })

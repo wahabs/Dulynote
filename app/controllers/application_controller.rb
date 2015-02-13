4e65d9ea7337910fc5f4a2a069d3f0eb91d9e53a
Wahab Sheikh <wahabmsheikh@gmail.com>
2015-02-13 10:13:17 -0800

    Add a more robust brand link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :brand_link
   protect_from_forgery with: :exception
 
   def current_user
     return nil unless session[:token]
     @current_user ||= User.find_by(session_token: session[:token])
+  end
+
+  def brand_link
+    if request.original_url == root_url && logged_in?
+      "/main#"
+    elsif request.original_url != root_url
+      "#"
+    else
+      root_url
+    end
   end
 
   def log_in_user!(user)
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,12 @@ u1 = User.create!(email: 'user@dulynote.com', password: 'vat9oryqbo0ater')
 
 nb1 = u1.notebooks.create!(title: "Dulynote Demo")
 nb2 = u1.notebooks.create!(title: "Programming Notes")
-nb3 = u1.notebooks.create!(title: "Random")
+nb3 = u1.notebooks.create!(title: "Stuff to Read")
 
 
 n1 = nb1.notes.create!(
   title: "Hello.",
-  body:  "Click on my title, body, and/or sticker to edit!<p><br>Highlight\n         some text and click one of the navbar buttons to apply formatting.\n         </p><p>We can <b>bold, </b><i> i<i>talicize, </i></i>and<i><i>\n         </i></i> <u>underline</u>. The eraser removes this type of formatting.\n         &nbsp;</p><ol><li>Here's a</li><li>numbered list</li><li>just for you!\n         </li></ol><ul><li>But what about</li><li>all these</li><li>bullets though?\n         </li></ul>Use the text resize button to create headers, and the \"code\" button\n         to insert preformatted text.<br><h3>How to do Merge Sort</h3><pre>def mergeSort(arr)<br>  arr.sort # lol<br>end<br></pre><br> You can  create notebooks, tag\n          notes, search notes, swap a note's notebook or sticker, and a lot more. Click\n          around, make some notes, enjoy yourself.<p><br></p>",
+  body:  "Click on my title, body, and/or sticker to edit!<p><br>Highlight\n         some text and click one of the navbar buttons to apply formatting.\n         </p><p>We can <b>bold, </b><i> i<i>talicize, </i></i>and<i><i>\n         </i></i> <u>underline</u>. The eraser removes this type of formatting.\n         &nbsp;</p><ol><li>Here's a</li><li>numbered list</li><li>just for you!\n         </li></ol><ul><li>But how about</li><li>all these</li><li>bullets though?\n         </li></ul>Use the text resize button to create headers, and the \"code\" button\n         to insert preformatted text.<br><h3>How to do Merge Sort</h3><pre>def mergeSort(arr)<br>  arr.sort # lol<br>end<br></pre><br> You can  create notebooks, tag\n          notes, search notes, swap a note's notebook or sticker, and a lot more. Click\n          around, make some notes, enjoy yourself.<p><br></p>",
   sticker: "star"
 )
 
@@ -17,26 +17,29 @@ n2 = nb1.notes.create!(
   sticker: "bookmark"
 )
 
-n3 = nb2.notes.create!(
-  title: "Ruby Procs",
-  body: "<p>Ruby isn't really made with functional programming in mind, but it can still be simulated via procs/blocks and lambdas. They are called closures in other languages, because they enclose variables in their immediate non-local scope. Think of a Proc object as an anonymous method, i.e. a section of code that can be passed around and called from other methods. A block is the code written in the Proc, the Proc is the actual object. Here's an example:</p><pre>class Array<br>&nbsp;&nbsp;def iterate(prc)<br>&nbsp;&nbsp;&nbsp; self.each_with_index do |el, i|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; self[i] = prc.call(el)<br>&nbsp;&nbsp;&nbsp; end<br>&nbsp; end<br>end<br><br>my_array = [0, 1, 2, 3]<br>cube = Proc.new { n ** 3 }<br><br>puts my_array.iterate(cube) # =&gt; [0, 1, 8, 27]</pre><p><i><b>Note: <i></i></b></i>This is a good resource:&nbsp;https://www.youtube.com/watch?v=VBC-G6hahWA</p><p><br></p>",
-  sticker: "diamond"
-)
-
-n4 = nb2.notes.create!(
-  title: "HTTP State/Sessions",
-  body:   "<h3>Protocol State<br></h3><p>HTTP is a <i>stateless protocol,</i> meaning that each request a client makes is independent of any other request (i.e. no information is carried across requests). While this simplicity allows for simplicity/optimization, it presents the problem of keeping track of certain necassary constants. For example, if on Amazon a user logs in and then requests their shopping cart, we need some way to keep track of the user so that we know which shopping cart to display. We do this by storing a <i>session</i>, which is just a resource that contains information about a user.</p><h3>Saving a session can be done by...</h3><ul><li>Store the data in an HTTP cookie: this is the default. A simple way to implement  this is to store a random string in the Rails `session` hash.</li><li>Store all session data in the server's database. Instead of setting the cookie with the data, only send the database key (the id of the session data in the database) to the client. On subsequent requests the client ships the key. Rails looks up this key in the database when session data is requested.</li><li>Store the data in the Rails cache.</li></ul><b>Source:</b>&nbsp;http://guides.rubyonrails.org/action_controller_overview.html#session",
-  sticker: "paper-plane"
-)
-
-n5 = nb3.notes.create!(
+n3 = nb1.notes.create!(
   title: "Shopping List",
   body: "<ul><li>Milk</li><li>Eggs<ul><li>dsfsdf</li></ul></li><li>Cheese<ul><li>dfdf </li></ul></li><li>Yogurt</li><li></li></ul>",
   sticker: "money"
 )
 
+n4 = nb2.notes.create!(
+  title: "Ruby Procs",
+  body: "<p>Ruby isn't really made with functional programming in mind, but it can still be simulated via procs/blocks and lambdas. They are called closures in other languages, because they enclose variables in their immediate non-local scope. Think of a Proc object as an anonymous method, i.e. a section of code that can be passed around and called from other methods. A block is the code written in the Proc, the Proc is the actual object. Here's an example:</p><pre>class Array<br>&nbsp;&nbsp;def iterate(prc)<br>&nbsp;&nbsp;&nbsp; self.each_with_index do |el, i|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; self[i] = prc.call(el)<br>&nbsp;&nbsp;&nbsp; end<br>&nbsp; end<br>end<br><br>my_array = [0, 1, 2, 3]<br>cube = Proc.new { n ** 3 }<br><br>puts my_array.iterate(cube) # =&gt; [0, 1, 8, 27]</pre><p><i><b>Note: <i></i></b></i>This is a good resource -&nbsp;https://www.youtube.com/watch?v=VBC-G6hahWA</p><p><br></p>",
+  sticker: "diamond"
+)
 
-n6 = nb3.notes.create!(title: "Note C2", body: "Body of C2", sticker: "calendar")
+n5 = nb2.notes.create!(
+  title: "HTTP State/Sessions",
+  body:   "<h3>Protocol State<br></h3><p>HTTP is a <i>stateless protocol,</i> meaning that each request a client makes is independent of any other request (i.e. no information is carried across requests). While this simplicity allows for simplicity/optimization, it presents the problem of keeping track of certain necassary constants. For example, if on Amazon a user logs in and then requests their shopping cart, we need some way to keep track of the user so that we know which shopping cart to display. We do this by storing a <i>session</i>, which is just a resource that contains information about a user.</p><h3>Saving a session can be done by...</h3><ul><li>Store the data in an HTTP cookie: this is the default. A simple way to implement  this is to store a random string in the Rails `session` hash.</li><li>Store all session data in the server's database. Instead of setting the cookie with the data, only send the database key (the id of the session data in the database) to the client. On subsequent requests the client ships the key. Rails looks up this key in the database when session data is requested.</li><li>Store the data in the Rails cache.</li></ul><b>Source:</b>&nbsp;http://guides.rubyonrails.org/action_controller_overview.html#session",
+  sticker: "paper-plane"
+)
+
+n6 = nb3.notes.create!(
+  title: "If",
+  body: "<i></i><p><br></p><p><i>If you can keep your head when all about you</i></p><p><i>Are losing theirs and blaming it on you,</i></p><p><i>   \nIf you can trust yourself when all men doubt you,\n</i></p><p><i>    But make allowance for their doubting too;</i></p><p><i>   \nIf you can wait and not be tired by waiting,</i></p><p><i>\n    Or being lied about, don’t deal in lies,</i></p><p><i>\nOr being hated, don’t give way to hating,</i></p><p><i>\n    And yet don’t look too good, nor talk too wise:</i></p><p><i>\n\nIf you can dream—and not make dreams your master;</i></p><p><i>   \n    If you can think—and not make thoughts your aim;</i></p><p><i>   \nIf you can meet with Triumph and Disaster</i></p><p><i>\n    And treat those two impostors just the same;</i></p><p><i>   \nIf you can bear to hear the truth you’ve spoken</i></p><p><i>\n    Twisted by knaves to make a trap for fools,</i></p><p><i>\nOr watch the things you gave your life to, broken,\n</i></p><p><i>    And stoop and build ’em up with worn-out tools:</i></p><p><i>\n\nIf you can make one heap of all your winnings\n</i></p><p><i>    And risk it on one turn of pitch-and-toss,</i></p><p><i>\nAnd lose, and start again at your beginnings</i></p><p><i>\n    And never breathe a word about your loss;</i></p><p><i>\n\nIf you can talk with crowds and keep your virtue</i></p><p><i> Or walk with Kings - nor lose the common touch,</i></p><p><i>\nIf neither foes nor loving friends can hurt you,</i></p><p><i>\n    If all men count with you, but none too much;</i></p><p><i>\nIf you can fill the unforgiving minute</i></p><p><i>With sixty seconds’ worth of distance run,&nbsp;</i></p><p><i>Yours is the Earth and everything that’s in it,</i></p><p><i>   \n    And—which is more—you’ll be a Man, my son!</i></p><p>- Rudyard Kipling</p><p><br></p>",
+  sticker: "moon-o"
+)
 
 
 t1 = u1.tags.create!(label: "Everything")
